18502a5e34879a9528b4fa98f9ba6c750a0703ea
Dawid Braun <braunndawid1600@gmail.com>
2026-03-02 10:20:38 +0100

    Zaaktualizowane readme, path.py.template, requirements i bat

--- ewidencja.bat
+++ ewidencja.bat
@@ -2,11 +2,13 @@
 echo Uruchamianie monitora plikow...
 
 :: Przejdź do folderu, w którym znajduje się Twój plik monitor.py
-:: Zastąp poniższą ścieżkę właściwą lokalizacją pliku monitor.py!
-cd /d "C:\\Programowanie\\auto_ewidencja"
+cd /d "C:\Programowanie\auto_ewidencja"
 
-:: Uruchom skrypt
+:: 1. Aktywuj wirtualne środowisko (.venv)
+call .venv\Scripts\activate.bat
+
+:: 2. Uruchom skrypt (teraz użyje Pythona z .venv)
 python monitor.py
 
-:: Zablokuj zamknięcie okna w przypadku błędu (pozwoli to przeczytać komunikat)
+:: Zablokuj zamknięcie okna w przypadku błędu
 pause
